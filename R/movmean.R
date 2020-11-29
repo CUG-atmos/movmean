9cfc247cd9a83268290187a5572a01e11dd62092
wlh006563 <weilh@cug.edu.cn>
2020-11-29 20:49:46 +0800

    20181000363 韦李宏

--- R/movmean.R
+++ R/movmean.R
@@ -1,4 +1,21 @@
-#' @export 
-movmean_R <- function(x, halfwin) {
-
+#' @export
+movmean_R <- function(x,n) { #n=halfwin
+  len <- length(x)
+  each.mean <- numeric()     #restore mean value of each movement
+  for( i in 1:length(x)){
+    if(n+1 <= i <= len-n){
+      ind <- (i-n):(i+n)
+      each.mean[length(each.mean)+1] <- mean(x[ind])
+    }
+    if( i < (n+1) ){
+      ind <- 1:(i+n)
+      each.mean[length(each.mean)+1] <- mean(x[ind])
+    }
+    if( i > (len-n)){
+      ind <- (i-n):len
+      each.mean[length(each.mean)+1] <- mean(x[ind])
+    }
+  }
+  movmean <- mean(each.mean)
+  return(movmean)
 }
