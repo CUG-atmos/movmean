d3b07adcf2944640935aac3f27e7afa8706ec094
liurh416 <18872599968@163.com>
2020-11-30 01:20:58 +0800

    20181003682柳睿涵

--- R/movmean.R
+++ R/movmean.R
@@ -1,15 +1,16 @@
 #' @export
 movmean_R <- function(x, halfwin) {
-  m <- halfwin
+  m <- halfwin    #halfwin为半窗长度
   win <- 2*m+1
-  y <- vector()
+  y <- vector()    #为返回值做准备
   for (i in 1:n){
-    if (i<m+1){
+    if (i<m+1){     #左边不满足m个
       ind <- (1:(i+m))
       y[i] <- sum(x[ind])/length(ind)
-    }else if(m+1<=i&i<=n-m){
-      y[i] <- sum(x[(i-m):(i+m)])/win
-    }else{
+    }else if(m+1<=i&i<=n-m){  #左右均满足
+      ind <- ((i-m):(i+m))
+      y[i] <- sum(x[ind])/win
+    }else{                    #右边不满足m个：i>(n-m)
       ind <- ((i-m):n)
       y[i] <- sum(x[ind])/length(ind)
     }
