e57e14875ce0344d911dc7b4a798a56d423771ab
lexin-lyw <1228808946@qq.com>
2020-11-29 23:53:41 +0800

    20181002557 刘云蔚

--- R/movmean.R
+++ R/movmean.R
@@ -1,4 +1,22 @@
-#' @export 
+#' @export
 movmean_R <- function(x, halfwin) {
-
+  m<-halfwin
+  n<-length(x)
+  win<-2*m+1
+  r<-vector()
+  for(i in 1:n){
+    if(i<=n-m&i>=m+1){
+      ind<-(i-m):(i+m)
+      r[i]<-sum(x[ind])/win
+    }
+    if(i<m+1){
+      ind<-i:(i+m)
+      r[i]<-sum(x[ind])/length(ind)
+    }
+    if(i>n-m){
+      ind<-(i-m):n
+      r[i]<-sum(x[ind])/length(ind)
+    }
+  }
+  return(r)
 }
