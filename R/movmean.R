df8c70191d193030ff527221e0df2b936f067860
Oona0501 <wuxinfeng@cug.cdu.cn>
2020-11-29 22:41:58 +0800

    20181002714武心凤

--- R/movmean.R
+++ R/movmean.R
@@ -1,4 +1,18 @@
-#' @export 
-movmean_R <- function(x, halfwin) {
-
+movmean<-function(x,halfwin){
+  n<-length(x)
+  m<-halfwin
+  win<-2*m+1
+  r<-vector()
+  for(i in 1:n){
+    if(i<m+1)
+      ind<-1:(i+m)
+    r[i]<-sum(x[ind])/length(ind)
+    if(m+1<=i&i<=n-m)
+      ind<-(i-m):(i+m)
+    r[i]<-sum(x[ind])/win
+    if(i>n-m)
+      ind<-(i-m):n
+    r[i]<-sum(x[ind])/length(ind)
+  }
+  return(r)
 }
