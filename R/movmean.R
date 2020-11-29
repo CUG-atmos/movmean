501d902323a701b8859fa9ee44bd0939f7389ca9
liurh416 <18872599968@163.com>
2020-11-30 01:12:05 +0800

    3

--- R/movmean.R
+++ R/movmean.R
@@ -4,13 +4,13 @@ movmean_R <- function(x, halfwin) {
   win <- 2*m+1
   y <- vector()
   for (i in 1:n){
-    if (i<(m+1)){
+    if (i<m+1){
       ind <- (1:(i+m))
       y[i] <- sum(x[ind])/length(ind)
-    }else if(m+1<=i<=n-m){
-      y[i] <- sum(x[i-m:i+m])/win
+    }else if(m+1<=i&i<=n-m){
+      y[i] <- sum(x[(i-m):(i+m)])/win
     }else{
-      ind <- (i-m:n)
+      ind <- ((i-m):n)
       y[i] <- sum(x[ind])/length(ind)
     }
   }
