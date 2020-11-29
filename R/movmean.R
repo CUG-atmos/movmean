65dd1fa1e1bea9626707f0672b4ada1f6a758b80
XiaoPing Kang <1258537709@qq.com>
2020-11-29 22:42:03 +0800

    20181002220康晓萍

--- R/movmean.R
+++ R/movmean.R
@@ -1,4 +1,22 @@
-#' @export 
+#' @export
 movmean_R <- function(x, halfwin) {
-
+  n<-length(x)
+  m<-halfwin
+  win<-2*m+1
+  r<-vector()
+  for (i in 1:n) {
+    if(i>=m&i<=n){
+      inds<-(i-m):(i+m)
+      r[i]<-x[inds]/win
+    }
+    if(i<m+1){
+      inds<-1:(i+m)
+      r[i]<-sum(x[inds])/length(inds)
+    }
+    if(i>n-m){
+      inds<-(i-m):n
+      r[i]<-sum(x[inds])/length(inds)
+    }
+  }
+  return(r)
 }
