efcffd1343da7e866903c5688beb3a8d7ca8d03d
cug-zmm <1291135251@qq.com>
2020-11-29 19:55:26 +0800

    20181000900 朱明铭

--- python/hello-numpy.py
+++ python/hello-numpy.py
@@ -1,10 +1,18 @@
 import numpy as np
 
 
-def hello():
-    print("hello world")
+def ma(data, ws):
+    v = np.cumsum(np.insert(data,0,0))
+    ma_v = (v[ws:]-v[:-ws]) /ws
+    return ma_v
     
 
+data=[1,2,3,4,5]
+a=ma(data,2)
 # x = np.array([1, 2, 3, 4])
 x = [1, 2, 3, 4]
-print(x[1])
+print(ma(x,2))
+    
+    
+
+
