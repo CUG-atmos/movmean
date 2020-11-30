f0aa79d72235ce44697fe4fe6b952b523d34e5d7
KittyandTeddy <1050739517@qq.com>
2020-11-30 16:59:23 +0800

    20181000808-肖劢谦

--- python/movmean.py
+++ python/movmean.py
@@ -1,39 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 29 19:15:44 2020
-
-@author: lcx
-"""
-# %%
-import numpy as np
-
-
-def movmean_py(x, half_win = 1):
-    '''           
-    x = np.array([1,4,5,9,2,3,4])
-    movmean_python(x,2)   #函数检验         
-    '''
-    m = half_win
-    win = 2*m+1
-    n = len(x)
-    y = np.zeros(n)
-    for i in range(0,n):
-        if i < m:
-            ind = np.arange(i+m+1)
-            y[i] = sum(x[ind])/len(x[ind])
-        elif i >= m and i <=(n-m-1):
-            ind = np.arange((i-m),(i+m+1))
-            y[i] = sum(x[ind])/win
-        else:
-            ind = np.arange((i-m),n)
-            y[i] = sum(x[ind])/len(x[ind])
+#This is a little function for moving average calculation in python.
+def movmean(x,m): #x:array,m:length of half window.
+    import numpy as np
+    n=len(x)
+    i=1
+    y=np.zeros(n)
+    while i<=n:
+        if isinstance(m,int)==False:     #In order to check the type of m and make sure it's an int.
+            print('Please input an integer as the value for m!!!')
+            break
+        if i<m+1:
+            ind=np.arange(0,(i+m),1)
+            if i+m>n:                    #For the reasonable result in case of a value of m which 
+                ind=np.arange(0,n,1)     #makes i+m larger than n when i is smaller than m+1.
+            y[i-1]=np.sum(x[ind])/len(ind)
+        elif m+1<=i<=n-m:
+            y[i-1]=np.sum(x[(i-1-m):(i+m)])/(2*m+1)
+        elif i>n-m:
+            ind=np.arange((i-m-1),n,1)
+            y[i-1]=np.sum(x[ind])/len(ind)
+        i+=1    
     return y
 
-
-# if __name__ == "__main__":
-#     x = np.array([1, 2, 3, 4])
-#     r = movmean_py(x, 1)
-#     print(r)
-
-
-# %%
