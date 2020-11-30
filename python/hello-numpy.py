ab4d53778a2262dc81eacac54fd3654ad848c83f
cug-zmm <1291135251@qq.com>
2020-11-30 23:02:43 +0800

    20181000900 朱明铭

--- python/hello-numpy.py
+++ python/hello-numpy.py
@@ -1,16 +1,28 @@
 import numpy as np
+def movemean(x,halfwin):
+    n=len(x)
+    p=np.zeros(n)
+    if halfwin>n/2:
+        return print("error:halfwin is too long.")
 
-#ws为窗口宽度
-def ma(data, ws):
-    v = np.cumsum(np.insert(data,0,0))
-    ma_v = (v[ws:]-v[:-ws]) /ws
-    return ma_v
-    
+    if halfwin==1:
+        p[0]=(p[0]+p[1])/2
+        for i in range(1,n-1):
+            p[i]=(p[i]+p[i-1]+p[i+1])/3
+            p[n-1]=(p[n-2]+p[n-1])/2
+    else:
+        m=halfwin
+        for i in range(0,n):
+            if i<m:
+                inds=np.arange(0,(i+m)+1)
+                p[i]=(np.sum(x[inds]))/len(inds)
+            elif i>=m and i<=n-m-1:
+                p[i]=(np.sum(x[i-m:i+m+1]))/(2*m+1)
+            else:
+                inds=np.arange(i-m,n)
+                p[i]=(np.sum(x[inds]))/len(inds)
+    return p
 
-
-# x = np.array([1, 2, 3, 4])
-x = [1, 2, 3, 4]
-print(ma(x,2))
     
     
 
