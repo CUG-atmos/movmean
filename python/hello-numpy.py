1f674ca3ef2e9bcfb6ed4233982177fc759e2bff
cug-zmm <1291135251@qq.com>
2020-11-30 22:37:10 +0800

    20181000900 朱明铭

--- python/hello-numpy.py
+++ python/hello-numpy.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-
+#ws为窗口宽度
 def ma(data, ws):
     v = np.cumsum(np.insert(data,0,0))
     ma_v = (v[ws:]-v[:-ws]) /ws
     return ma_v
     
 
-data=[1,2,3,4,5]
-a=ma(data,2)
+
 # x = np.array([1, 2, 3, 4])
 x = [1, 2, 3, 4]
 print(ma(x,2))
