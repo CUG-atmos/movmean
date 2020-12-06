b014811d805b44c0933253c30e7d9c3303a83eed
PengMaoYangZong <75171477+PengMaoYangZong@users.noreply.github.com>
2020-12-06 23:32:02 +0800

    20171002974 彭茂央宗

--- R/movmean.R
+++ R/movmean.R
@@ -1,29 +1,13 @@
-# movmean_R函数检验
-# x <- runif(100,50,100)
-# movmean <- movmean_R(x,5)
-# x1<-1:100
-# plot(x1,x,type='o')#未滑动平均数据
-# plot(x1,movmean,type='o')#滑动平均后数据
-
-#' @export
-movmean_R <- function(x, halfwin) {
-  m <- halfwin
-  n <- length(x)
-  window <- 2 * m + 1
-  r <- vector()
-  for (i in 1:n) {
-    if (i >= (m + 1) & i <= (n - m)) {
-      ind <- (i - m):(i + m)
-      r[i] <- sum(x[ind]) / window
-    }
-    else if (i < (m + 1)) {
-      ind <- 1:(i + m)
-      r[i] <- sum(x[ind]) / length(ind)
-    }
-    else if (i > (n - m)) {
-      ind <- (i - m):n
-      r[i] <- sum(x[ind]) / length(ind)
-    }
-  }
-  r
-}
+library(smooth)
+library(greybox)
+sma(M3$N2457$x, h=18, silent=FALSE)
+sma(M3$N2568$x, h=18, interval=TRUE)
+x <- c(1,3,6,10,20,22,16,12,18,24,42,8,16,15)
+x <- data.frame(x)
+colnames(x) <- "pm"
+mav <- function(x,n=5){stats::filter(x,rep(1/n,n), sides=1)}
+mav(x,1)
+mav(x,2)
+mav(x,3)
+mav(x,4)
+mav(x,5)
